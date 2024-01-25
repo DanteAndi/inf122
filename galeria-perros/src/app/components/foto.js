370dff40b1d842cd7c27236cb7e7a739b97a2164
Dreyfuss <119564437+DanteAndi@users.noreply.github.com>
2024-01-25 15:31:19 +0000

    pika

--- galeria-perros/src/app/components/foto.js
+++ galeria-perros/src/app/components/foto.js
@@ -6,17 +6,22 @@ import { useEffect, useState } from "react";
 function foto(){
     const[perrito,setPerrito]=useState("/vercel.svg");
     const[estado, setEstado]=useState("esperando")
+    const[habilidad, setHabilidad]=useState("habilidad")
     const url="https://pokeapi.co/api/v2/pokemon/raichu";
     useEffect(()=>{
         fetch(url).then(res=>res.json())
         .then(data=>{
-            setPerrito(data.sprites.front_default)}); 
+            setPerrito(data.sprites.front_default)
+            setEstado(data.species.name)
+            setHabilidad(data.abilities[0].ability.name)
+        }); 
 
     },[]);
     return(
         <div className={style.contenedor}>
             <h1>{estado}</h1>
             <Image src={perrito}alt="dog" width={500} height={500}/>
+            <h1>{habilidad}</h1>
         </div>
     )
 }
